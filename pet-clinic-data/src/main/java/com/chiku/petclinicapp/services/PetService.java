eb59ba7c73fc562d122e61e4b3faf2028654979e
chikuBanda <chikubanda.cb@gmail.com>
2020-04-29 20:59:56 +0000

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/com/chiku/petclinicapp/services/PetService.java
+++ pet-clinic-data/src/main/java/com/chiku/petclinicapp/services/PetService.java
@@ -4,11 +4,4 @@ import com.chiku.petclinicapp.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-}
+public interface PetService extends CrudService<Pet, Long> {}
--- pet-clinic-data/src/main/java/com/chiku/petclinicapp/services/VetService.java
+++ pet-clinic-data/src/main/java/com/chiku/petclinicapp/services/VetService.java
@@ -4,11 +4,4 @@ import com.chiku.petclinicapp.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
-}
+public interface VetService extends CrudService<Vet, Long>{}
